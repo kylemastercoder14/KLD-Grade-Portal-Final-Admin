f1430bef1e120cb771147032d31da2facd30a502
Kyle Andre David Lim <kyleandrelim17@gmail.com>
2024-12-03 01:39:09 +0800

    added some card in dashboard

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -1,21 +1,88 @@
-import Image from "next/image";
 import React from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import db from "@/lib/db";
+import { BadgeCheck, BadgeX, LibraryBig, Users } from "lucide-react";
 
-const Dashboard = () => {
+const Dashboard = async () => {
+  const students = await db.students.findMany();
+  const assignedAdvisers = await db.assignTeacher.findMany();
+  const grades = await db.grades.findMany();
+
+  const totalGrades = grades.length;
+  const passingCount = grades.filter(
+    (grade) => grade.remarks === "PASSED"
+  ).length;
+  const failingCount = grades.filter(
+    (grade) => grade.remarks === "FAILED"
+  ).length;
+
+  const passingRate =
+    totalGrades > 0 ? ((passingCount / totalGrades) * 100).toFixed(2) : 0;
+  const failingRate =
+    totalGrades > 0 ? ((failingCount / totalGrades) * 100).toFixed(2) : 0;
   return (
-    <div className="flex flex-col h-[50vh] items-center justify-center">
-      <Image
-        src="/images/under-construction.png"
-        alt="Maintenance"
-        width={300}
-        height={300}
-      />
-      <h1 className="text-5xl font-bold">Dashboard Page</h1>
-      <p className="text-md mt-2 text-muted-foreground">
-        This page is under construction. We are working hard to bring you new
-        features. Please check back soon!
-      </p>
-    </div>
+    <>
+      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium">
+              Total Students
+            </CardTitle>
+            <Users />
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold">{students.length}</div>
+            <p className="text-xs text-muted-foreground">
+              The total number of students currently enrolled in the system.
+            </p>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium">Passing Rate</CardTitle>
+            <BadgeCheck />
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold">{passingRate}%</div>
+            <p className="text-xs text-muted-foreground">
+              Percentage of students who have successfully passed their courses.
+            </p>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium">Failing Rate</CardTitle>
+            <BadgeX />
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold">{failingRate}%</div>
+            <p className="text-xs text-muted-foreground">
+              Percentage of students who did not meet the passing criteria.
+            </p>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium">
+              Advisory Classes
+            </CardTitle>
+            <LibraryBig />
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold">{assignedAdvisers.length}</div>
+            <p className="text-xs text-muted-foreground">
+              Total number of advisory classes assigned to teachers.
+            </p>
+          </CardContent>
+        </Card>
+      </div>
+    </>
   );
 };
 
